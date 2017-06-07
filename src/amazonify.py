dababf9294333cafe647e6a94739f7e44dc8a9ce
Danie Viljoen <danie.viljoen@orionhealth.com>
2017-06-08 07:25:25 +1200

    creating movie json file

--- src/amazonify.py
+++ src/amazonify.py
@@ -20,6 +20,7 @@ movieDB = {}
 movieList = []
 
 movieDir = sys.argv[1]
+destDir = sys.argv[2]
 print 'amazonfy movies in: ' + movieDir
 
 def extractProp(mailFile):
@@ -32,7 +33,7 @@ def extractProp(mailFile):
 def populateMovieDB(dirWithMovieDirs):
     for dirpath, dnames, fnames in os.walk(dirWithMovieDirs):
         for f in fnames:
-            print extractProp(f)
+            extractProp(f)
             break
 
 def populateMovieList():
@@ -44,15 +45,70 @@ def buildFileProp():
     numberOfMovies = len(movieList)
     print "NumberOfM: " + str(numberOfMovies)
     for key in movieDB.keys():
-        x=[randint(0,numberOfMovies) for p in range(0,15)]
+        x=[randint(0,numberOfMovies-1) for p in range(0,15)]
         otherNames = []
         for number in x:
             name = movieList[number]
             if (len(otherNames)<10 and name!=key):
                 otherNames.append(name)
-        print otherNames
+        value = movieDB[key]
+        movieDB[key]=(value[0], value[1], otherNames)
+
+def extractKey(fileName):
+    newName = fileName.replace('.jpg','')
+    for e in range(0,5):
+        endPart = '_' + str(e)
+        if newName.endswith(endPart):
+            newName = newName.replace(endPart,'')
+            break
+    return newName
+
+def makeMovieFile(toDir, values, posters, mainFile):
+    import json
+    posters.append(mainFile)
+    jsonDict = {}
+    jsonDict['name']=values[0]
+    jsonDict['names']=values[2]
+    jsonDict['posters']=posters
+    jsonFile = json.dumps(jsonDict)
+    fileName = values[1] + ".json"
+    f = open(fileName, 'w')
+    f.write(json.dumps(jsonDict))
+    f.flush()
+    f.close()
+    print jsonFile
+
+def makeAmazonFiles(fromDir, toDir):
+    os.mkdir(toDir)
+    for dirpath, dnames, fnames in os.walk(fromDir):
+        for f in fnames:
+            fileSrc = os.path.join(dirpath,f)
+            key = f.replace('.jpg','')
+            values = movieDB[key]
+            newFile = f.replace(key, values[1])
+            fileDest = os.path.join(toDir, newFile)
+            os.rename(fileSrc, fileDest)
+            posters = []
+            for index in range(0,5):
+                newSrc = key + "_" + str(index) + '.jpg'
+                fileSrc_ = os.path.join(dirpath, newSrc)
+                newDestination = newSrc.replace(key, values[1])
+                fileDest_ = os.path.join(toDir, newDestination)
+                posters.append(fileDest_)
+                print fileSrc_
+                print fileDest_
+                os.rename(fileSrc_, fileDest_)
+
+            makeMovieFile(toDir, values, posters, fileDest)
+            break
+
+
+
+
 
 
 populateMovieDB(movieDir)
 populateMovieList()
 buildFileProp()
+
+makeAmazonFiles(movieDir, destDir)
